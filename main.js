5e12343cb8419c40bd802104e5e9cb438fdadb68
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-06-24 09:50:15 +0200

    fixed showing sections
    fixed showing sections

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 // script.js
 $(document).ready(function() {
-  function loadData(sectionId, dataKey) {
-    $.getJSON('cv.json', function(data) {
-      var sectionContent = $('#' + sectionId + '-content');
-      sectionContent.text(data[dataKey]);
-    });
+  // Funkcja do ukrywania wszystkich sekcji
+  function hideAllSections() {
+    $('.content section').hide();
   }
 
   // Pobierz dane na początku
@@ -16,6 +14,26 @@ $(document).ready(function() {
   $('nav a').click(function(e) {
     e.preventDefault();
     var sectionId = $(this).attr('href').substring(1); // Pobierz ID sekcji bez '#'
-    loadData(sectionId, sectionId.replace('-', '_'));
+
+    // Ukryj wszystkie sekcje
+    hideAllSections();
+
+    // Pokaż tylko wybraną sekcję
+    $('#' + sectionId).show();
+
+    // Wczytaj dane dla wybranej sekcji
+    loadData(sectionId);
   });
+
+  // Funkcja do wczytywania danych dla danej sekcji
+  function loadData(sectionId) {
+    $.getJSON('cv.json', function(data) {
+      var sectionContent = $('#' + sectionId + '-content');
+      var dataKey = sectionId.replace('-', '_');
+      sectionContent.text(data[dataKey]);
+    });
+  }
+
+  // Ukryj wszystkie sekcje po załadowaniu strony
+  hideAllSections();
 });
