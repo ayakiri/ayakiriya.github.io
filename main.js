e3232b0d4597102702f36577c76c6aae2e298d33
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-06-24 09:42:41 +0200

    different js
    different js

--- main.js
+++ main.js
@@ -1,50 +1,21 @@
 // script.js
 $(document).ready(function() {
-  // Pobierz dane z pliku JSON
+  function loadData(sectionId, dataKey) {
+    $.getJSON('cv.json', function(data) {
+      var sectionContent = $('#' + sectionId + '-content');
+      sectionContent.text(data[dataKey]);
+    });
+  }
+
+  // Pobierz dane na początku
   $.getJSON('cv.json', function(data) {
-    // Uzupełnij sekcję "Kim jestem"
     $('#kim-jestem-content').text(data.kim_jestem);
+  });
 
-    // Uzupełnij sekcję "Edukacja"
-    var edukacjaList = $('#edukacja-list');
-    $.each(data.edukacja, function(index, item) {
-      var listItem = $('<li>').text(item);
-      edukacjaList.append(listItem);
-    });
-
-    // Uzupełnij sekcję "Projekty"
-    var projektyList = $('#projekty-list');
-    $.each(data.projekty, function(index, item) {
-      var listItem = $('<li>').text(item);
-      projektyList.append(listItem);
-    });
-
-    // Uzupełnij sekcję "Doświadczenie"
-    var doswiadczenieList = $('#doswiadczenie-list');
-    $.each(data.doswiadczenie, function(index, item) {
-      var listItem = $('<li>').text(item);
-      doswiadczenieList.append(listItem);
-    });
-
-    // Uzupełnij sekcję "Umiejętności"
-    var umiejetnosciList = $('#umiejetnosci-list');
-    $.each(data.umiejetnosci, function(index, item) {
-      var listItem = $('<li>').text(item);
-      umiejetnosciList.append(listItem);
-    });
-
-    // Uzupełnij sekcję "Języki"
-    var jezykiList = $('#jezyki-list');
-    $.each(data.jezyki, function(index, item) {
-      var listItem = $('<li>').text(item);
-      jezykiList.append(listItem);
-    });
-
-    // Uzupełnij sekcję "Certyfikaty"
-    var certyfikatyList = $('#certyfikaty-list');
-    $.each(data.certyfikaty, function(index, item) {
-      var listItem = $('<li>').text(item);
-      certyfikatyList.append(listItem);
-    });
+  // Obsługa kliknięcia sekcji
+  $('nav a').click(function(e) {
+    e.preventDefault();
+    var sectionId = $(this).attr('href').substring(1); // Pobierz ID sekcji bez '#'
+    loadData(sectionId, sectionId.replace('-', '_'));
   });
 });
