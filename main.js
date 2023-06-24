615822a8d6d186fab0b3de8e3b2070b996542ad4
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-06-24 11:41:13 +0200

    json update?
    json update?

--- main.js
+++ main.js
@@ -1,30 +1,101 @@
 $(document).ready(function() {
-  // Function to hide all sections
   function hideAllSections() {
     $('.content section').hide();
   }
 
-  // Fetch data on initial load
   $.getJSON('cv.json', function(data) {
     $('#about-me-content').text(data.about_me);
   });
 
-  // Handling section clicks
-  $('.nav-link').click(function(e) {
-    e.preventDefault();
-    var sectionId = $(this).attr('href').substring(1); // Get section ID without '#'
 
-    // Hide all sections
-    hideAllSections();
+  function loadAboutMeContent(data) {
+    $('#about-me-content').text(data.about_me);
+  }
+    
+  function loadEducationContent(data) {
+    var educationContent = '';
+    data.education.forEach(function(edu) {
+    educationContent += '<div class="education-item">' +
+    '<h3>' + edu.name + '</h3>' +
+    '<p class="date">' + edu.dates + '</p>' +
+    '<p>' + edu.course + '</p>' +
+    '<ul>';
+    edu.extra_info.forEach(function(info) {
+    educationContent += '<li>' + info + '</li>';
+    });
+    educationContent += '</ul></div>';
+    });
+    $('#education-content').html(educationContent);
+  }
+  
+  function loadProjectsContent(data) {
+    var projectsContent = '';
+    data.projects.forEach(function(proj) {
+    projectsContent += '<div class="project-item">' +
+    '<h3>' + proj.name + '</h3>' +
+    '<p class="date">' + proj.dates + '</p>' +
+    '<p>' + proj.link + '</p>' +
+    '<ul>';
+    proj.extra_info.forEach(function(info) {
+    projectsContent += '<li>' + info + '</li>';
+    });
+    projectsContent += '</ul></div>';
+    });
+    $('#projects-content').html(projectsContent);
+  }
+  
+  function loadExperienceContent(data) {
+    var experienceContent = '';
+    data.experience.forEach(function(exp) {
+    experienceContent += '<div class="experience-item">' +
+    '<h3>' + exp.title + '</h3>' +
+    '<p class="date">' + exp.dates + '</p>' +
+    '<p>' + exp.position + '</p>' +
+    '<ul>';
+    exp.extra_info.forEach(function(info) {
+    experienceContent += '<li>' + info + '</li>';
+    });
+    experienceContent += '</ul></div>';
+    });
+    $('#experience-content').html(experienceContent);
+  }
+  
+  function loadSkillsContent(data) {
+    var skillsContent = '<div class="hard-skills"><h4>Hard Skills:</h4><ul>';
+    data.skills[0].hard_skills.forEach(function(skill) {
+    skillsContent += '<li>' + skill + '</li>';
+    });
+    skillsContent += '</ul></div><div class="soft-skills"><h4>Soft Skills:</h4><ul>';
+    data.skills[0].soft_skills.forEach(function(skill) {
+    skillsContent += '<li>' + skill + '</li>';
+    });
+    skillsContent += '</ul></div>';
+    $('#skills-content').html(skillsContent);
+  }
+  
+  function loadLanguagesContent(data) {
+    var languagesContent = '';
+    data.languages.forEach(function(lang) {
+    languagesContent += '<div class="language-item">' +
+    '<p class="language">' + lang.language + '</p>' +
+    '<p class="level">' + lang.proficiency + '</p>' +
+    '</div>';
+    });
+    $('#languages-content').html(languagesContent);
+  }
+  
+  function loadCertificationsContent(data) {
+    var certificationsContent = '';
+    data.certifications.forEach(function(cert) {
+    certificationsContent += '<div class="certification-item">' +
+    '<h3>' + cert.name + '</h3>' +
+    '<p>' + cert.date + '</p>' +
+    '<p>' + cert.examinator + '</p>' +
+    '</div>';
+    });
+    $('#certifications-content').html(certificationsContent);
+  }
 
-    // Show only the selected section
-    $('#' + sectionId).show();
-
-    // Load data for the selected section
-    loadData(sectionId);
-  });
-
-  // Function to load data for a specific section
   function loadData(sectionId) {
     $.getJSON('cv.json', function(data) {
       var sectionContent = $('#' + sectionId + '-content');
@@ -34,79 +105,49 @@ $(document).ready(function() {
           sectionContent.text(data.about_me);
           break;
 
-        case 'education':
-          var educationContent = '';
-          data.education.forEach(function(edu) {
-            educationContent += '<div class="education-item">' +
-              '<h3>' + edu.name + '</h3>' +
-              '<p class="date">' + edu.dates + '</p>' +
-              '<p>' + edu.description + '</p>' +
-              '</div>';
-          });
-          sectionContent.html(educationContent);
-          break;
-
-        case 'projects':
-          var projectsContent = '';
-          data.projects.forEach(function(proj) {
-            projectsContent += '<div class="project-item">' +
-              '<h3>' + proj.name + '</h3>' +
-              '<p class="date">' + proj.dates + '</p>' +
-              '<p>' + proj.description + '</p>' +
-              '</div>';
-          });
-          sectionContent.html(projectsContent);
-          break;
-
-        case 'experience':
-          var experienceContent = '';
-          data.experience.forEach(function(exp) {
-            experienceContent += '<div class="experience-item">' +
-              '<h3>' + exp.title + '</h3>' +
-              '<p class="date">' + exp.dates + '</p>' +
-              '<p>' + exp.description + '</p>' +
-              '</div>';
-          });
-          sectionContent.html(experienceContent);
-          break;
-
-        case 'skills':
-          var skillsContent = '';
-          data.skills.forEach(function(skill) {
-            skillsContent += '<p>' + skill + '</p>';
-          });
-          sectionContent.html(skillsContent);
-          break;
-
-        case 'languages':
-          var languagesContent = '';
-          data.languages.forEach(function(lang) {
-            languagesContent += '<div class="language-item">' +
-              '<p class="language">' + lang.language + '</p>' +
-              '<p class="level">' + lang.proficiency + '</p>' +
-              '</div>';
-          });
-          sectionContent.html(languagesContent);
-          break;
-
-        case 'certifications':
-          var certificationsContent = '';
-          data.certifications.forEach(function(cert) {
-            certificationsContent += '<div class="certification-item">' +
-              '<h3>' + cert.name + '</h3>' +
-              '<p>' + cert.date + '</p>' +
-              '</div>';
-          });
-          sectionContent.html(certificationsContent);
-          break;
-
-        default:
-          sectionContent.text('No data available.');
-          break;
-      }
-    });
+          case 'education':
+            loadEducationContent(data);
+            break;
+      
+          case 'projects':
+            loadProjectsContent(data);
+            break;
+      
+          case 'experience':
+            loadExperienceContent(data);
+            break;
+      
+          case 'skills':
+            loadSkillsContent(data);
+            break;
+      
+          case 'languages':
+            loadLanguagesContent(data);
+            break;
+      
+          case 'certifications':
+            loadCertificationsContent(data);
+            break;
+      
+          default:
+            $('#'+sectionId+'-content').text('No data available.');
+            break;
+        }
+      });
+      
   }
 
-  // Hide all sections on page load
-  hideAllSections();
+  $.getJSON('cv.json', function(data) {
+    loadAboutMeContent(data);
+    });
+    
+    $('.nav-link').click(function(e) {
+    e.preventDefault();
+    var sectionId = $(this).attr('href').substring(1);
+    hideAllSections();
+    $('#' + sectionId).show();
+    loadData(sectionId);
+    });
+    
+    hideAllSections();
 });
