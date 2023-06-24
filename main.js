6893770a18c3952b87da90c390f20fe887103975
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-06-24 09:39:08 +0200

    update
    update

--- main.js
+++ main.js
@@ -1,74 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      var cvData = JSON.parse(this.responseText);
-      displayCV(cvData);
-    }
-  };
-  xmlhttp.open("GET", "cv.json", true);
-  xmlhttp.send();
-});
+// script.js
+$(document).ready(function() {
+  // Pobierz dane z pliku JSON
+  $.getJSON('cv.json', function(data) {
+    // Uzupełnij sekcję "Kim jestem"
+    $('#kim-jestem-content').text(data.kim_jestem);
 
-function displayCV(cvData) {
-  var cvElement = document.getElementById("cv");
-  var loadingElement = document.getElementById("loading");
-  
-  loadingElement.style.display = "none";
-  cvElement.style.display = "block";
-  
-  var html = "<h2>Introduction</h2>";
-  html += "<p>" + cvData.introduction + "</p>";
-  
-  html += "<h2>Education</h2>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.education.length; i++) {
-    html += "<li>" + cvData.education[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  html += "<h2>Experience</h2>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.experience.length; i++) {
-    html += "<li>" + cvData.experience[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  html += "<h2>Projects</h2>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.projects.length; i++) {
-    html += "<li>" + cvData.projects[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  html += "<h2>Skills</h2>";
-  html += "<h3>Hard Skills</h3>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.skills.hard.length; i++) {
-    html += "<li>" + cvData.skills.hard[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  html += "<h3>Soft Skills</h3>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.skills.soft.length; i++) {
-    html += "<li>" + cvData.skills.soft[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  html += "<h2>Languages</h2>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.languages.length; i++) {
-    html += "<li>" + cvData.languages[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  html += "<h2>Certificates</h2>";
-  html += "<ul>";
-  for (var i = 0; i < cvData.certificates.length; i++) {
-    html += "<li>" + cvData.certificates[i] + "</li>";
-  }
-  html += "</ul>";
-  
-  cvElement.innerHTML = html;
-}
+    // Uzupełnij sekcję "Edukacja"
+    var edukacjaList = $('#edukacja-list');
+    $.each(data.edukacja, function(index, item) {
+      var listItem = $('<li>').text(item);
+      edukacjaList.append(listItem);
+    });
+
+    // Uzupełnij sekcję "Projekty"
+    var projektyList = $('#projekty-list');
+    $.each(data.projekty, function(index, item) {
+      var listItem = $('<li>').text(item);
+      projektyList.append(listItem);
+    });
+
+    // Uzupełnij sekcję "Doświadczenie"
+    var doswiadczenieList = $('#doswiadczenie-list');
+    $.each(data.doswiadczenie, function(index, item) {
+      var listItem = $('<li>').text(item);
+      doswiadczenieList.append(listItem);
+    });
+
+    // Uzupełnij sekcję "Umiejętności"
+    var umiejetnosciList = $('#umiejetnosci-list');
+    $.each(data.umiejetnosci, function(index, item) {
+      var listItem = $('<li>').text(item);
+      umiejetnosciList.append(listItem);
+    });
+
+    // Uzupełnij sekcję "Języki"
+    var jezykiList = $('#jezyki-list');
+    $.each(data.jezyki, function(index, item) {
+      var listItem = $('<li>').text(item);
+      jezykiList.append(listItem);
+    });
+
+    // Uzupełnij sekcję "Certyfikaty"
+    var certyfikatyList = $('#certyfikaty-list');
+    $.each(data.certyfikaty, function(index, item) {
+      var listItem = $('<li>').text(item);
+      certyfikatyList.append(listItem);
+    });
+  });
+});
